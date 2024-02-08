95a50d2e27e7912668d509888b50a5e97361e108
gab8192 <gabrilomba57@gmail.com>
2024-02-08 16:12:58 +0100

    Don't do lmr deeper/shallower at root
    Bench 1246841

--- src/search.cpp
+++ src/search.cpp
@@ -1160,12 +1160,14 @@ moves_loop:  // When in check, search starts here
             // Do a full-depth search when reduced LMR search fails high
             if (value > alpha && d < newDepth)
             {
-                // Adjust full-depth search based on LMR results - if the result
-                // was good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 49 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
+                if (!rootNode) {
+                    // Adjust full-depth search based on LMR results - if the result
+                    // was good enough search deeper, if it was bad enough search shallower.
+                    const bool doDeeperSearch    = value > (bestValue + 49 + 2 * newDepth);  // (~1 Elo)
+                    const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                    newDepth += doDeeperSearch - doShallowerSearch;
+                }
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
